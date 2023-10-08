3872fc8d52616f48cab0e7d0e25827f5156108fd
Augusto Omena <augusto.omena.n@gmail.com>
2023-10-08 20:21:46 -0300

    Criando conteúdo e organizando

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     new TypeIt(".animated", {
         speed: 200,
         loop: true
-    }).type('Software Developer', {delay: 900})
+    }).type('Software Developer', {delay: 2000})
       .move(-18, { delay: 100})
       .type('Java ', {delay: 900})
       .move(-1)
       .delete(5)
-      .type('Javascript', {delay: 1100})
+      .type('Javascript', {delay: 2000})
       .move(0)
       .delete(11)
     .go();
