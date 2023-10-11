e3d5759d7acbe347eee7d00978ce8bdcb69c12cb
Augusto Omena <augusto.omena.n@gmail.com>
2023-10-11 00:48:03 -0300

    adicionado botão tradutor

--- JS/app.js
+++ JS/app.js
@@ -25,3 +25,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // -------------------------------------- fim Carrossel ------------------------------------------------------- */
+
+document.addEventListener('DOMContentLoaded', function () {
+  const checkbox = document.getElementById('chk');
+  const translations = {
+    'pt': {
+      'projects-section': 'Projetos Recentes',
+      'h2': 'Olá! Eu sou o Augusto',
+      'p': 'Apaixonado pelo desenvolvimento web desde os 18 anos, brasileiro e nascido em 2003. Sou estudante de Análise e Desenvolvimento de Sistemas no Centro Universitário Senac Santo Amaro e Desenvolvedor na Secretaria de Desenvolvimento Econômico de Angra dos Reis. Sou uma pessoa que adora desafios, assim como todo desenvolvedor, mas em especial aqueles que me tiram da zona de conforto, pois as minhas maiores evoluções e aprendizados vieram de momentos em que me vi obrigado a sair dela e enfrentar a vida. Estudante assíduo, que aprecia tomar café nas horas vagas e ...'
+    },
+    'en': {
+      'projects-section': 'Recent Projects',
+      'h2': 'Hello! I am Augusto',
+      'p': 'Passionate about web development since the age of 18, Brazilian, and born in 2003. I am a student of Systems Analysis and Development at Centro Universitário Senac Santo Amaro and a Developer at the Secretariat of Economic Development of Angra dos Reis. I am someone who loves challenges, just like any developer, but especially those that take me out of my comfort zone, as my greatest advancements and learnings have come from moments when I was forced to leave it and face life. Diligent student who enjoys having coffee in his spare time and ...'
+    }
+  };
+
+  // Declare a variável projectsSection no escopo global
+  const projectsSection = document.querySelector('.projects.section h2');
+
+  checkbox.addEventListener('change', function () {
+    const language = checkbox.checked ? 'en' : 'pt';
+    translateContent(language);
+  });
+
+  function translateContent(language) {
+    const h2 = document.querySelector('.content-section h2');
+    const p = document.querySelector('.content-section p');
+
+    if (translations[language]) {
+      h2.innerText = translations[language]['h2'];
+      p.innerText = translations[language]['p'];
+      projectsSection.innerText = translations[language]['projects-section'];
+    }
+  }
+});
