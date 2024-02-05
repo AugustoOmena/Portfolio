cb333aea8f1e85cee23468cd4989c58a535dab48
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-02-05 15:39:30 -0300

    Tradução do titulo minhas habilidades e do primeiro botão "backend" do accordeon.

--- JS/app.js
+++ JS/app.js
@@ -27,18 +27,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.getElementById('chk');
   const translations = {
     'pt': {
-      'projects-section': 'Projetos Recentes',
       'h2': 'Olá! Eu sou o Augusto',
-      'p': 'Sou um Desenvolvedor Full Stack com experiência em programação para web, desktop e mobile. Participei de quatro edições do Grand Prix de Inovação no Senai, em uma quinta edição contribui. Essas experiências me permitiram desenvolver projetos desafiadores em equipe, fortalecendo não apenas minhas habilidades técnicas, mas também minha capacidade de colaborar em ambientes dinâmicos de desenvolvimento de software. Estou comprometido em buscar conhecimentos práticos contínuos, sempre procurando oportunidades para aplicar e aprimorar minhas habilidades técnicas. Estou preparado para enfrentar desafios, aprender constantemente e contribuir de maneira significativa para projetos que exijam expertise em programação Full Stack.'
+      'p': 'Sou um Desenvolvedor Full Stack com experiência em programação para web, desktop e mobile. Participei de quatro edições do Grand Prix de Inovação no Senai, em uma quinta edição contribui. Essas experiências me permitiram desenvolver projetos desafiadores em equipe, fortalecendo não apenas minhas habilidades técnicas, mas também minha capacidade de colaborar em ambientes dinâmicos de desenvolvimento de software. Estou comprometido em buscar conhecimentos práticos contínuos, sempre procurando oportunidades para aplicar e aprimorar minhas habilidades técnicas. Estou preparado para enfrentar desafios, aprender constantemente e contribuir de maneira significativa para projetos que exijam expertise em programação Full Stack.',
+      'tituloHabilidades': 'Minhas Habilidades',
+      'tituloCursos': 'Cursos de Aperfeiçoamento',
+      'CertificadosBackend': 'Backend - Aplicações Multi-Plataforma'
     },
     'en': {
       'projects-section': 'Recent Projects',
       'h2': 'Hello! I am Augusto',
-      'p': 'I am a Full Stack Developer with experience in web, desktop, and mobile programming. I have participated in four editions of the Innovation Grand Prix at Senai, and in a fifth edition, I made a contribution. These experiences have allowed me to work on challenging projects as part of a team, enhancing not only my technical skills but also my ability to collaborate in dynamic software development environments. I am committed to continuously seeking practical knowledge, always looking for opportunities to apply and enhance my technical skills. I am prepared to take on challenges, learn constantly, and contribute significantly to projects that demand expertise in Full Stack programming.'
+      'p': 'I am a Full Stack Developer with experience in web, desktop, and mobile programming. I have participated in four editions of the Innovation Grand Prix at Senai, and in a fifth edition, I made a contribution. These experiences have allowed me to work on challenging projects as part of a team, enhancing not only my technical skills but also my ability to collaborate in dynamic software development environments. I am committed to continuously seeking practical knowledge, always looking for opportunities to apply and enhance my technical skills. I am prepared to take on challenges, learn constantly, and contribute significantly to projects that demand expertise in Full Stack programming.',
+      'tituloHabilidades': 'My Skills',
+      'tituloCursos': 'Improvement courses',
+      'CertificadosBackend': 'Backend - Multi-Platform Applications'
     }
   };
-
-  const projectsSection = document.querySelector('.projects.section h2');
 
   checkbox.addEventListener('change', function () {
     const language = checkbox.checked ? 'en' : 'pt';
@@ -47,16 +50,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function translateContent(language) {
     const h2 = document.querySelector('.content-section h2');
-    const p = document.querySelector('.content-section p');
+    const p = document.querySelector('.content-section .minibiola');
+    const tituloHabilidades = document.querySelector('.containere .habilidades-title');
+    const tituloCursos = document.querySelector('.containere .cursos-title');
+    const CertificadosBackend = document.querySelector('.containere .certificadosBackend');
+
 
     if (translations[language]) {
       h2.innerText = translations[language]['h2'];
       p.innerText = translations[language]['p'];
-      projectsSection.innerText = translations[language]['projects-section'];
+      tituloHabilidades.innerText = translations[language]['tituloHabilidades'];
+      tituloCursos.innerText = translations[language]['tituloCursos'];
+      CertificadosBackend.innerText = translations[language]['CertificadosBackend'];
+
     }
   }
 });
 
+/* Formulário de mensagem - formato assíncrono */
 class FormSubmit {
   constructor(settings) {
     this.settings = settings;
